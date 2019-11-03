3679cc3a2c5b2de6d73ac6b7ef90b2297cc4957d
ItaloMatos10 <54958508+ItaloMatos10@users.noreply.github.com>
2019-11-03 00:11:39 -0300

    Add files via upload

--- EDB-atvd10.c
+++ EDB-atvd10.c
@@ -25,16 +25,37 @@ void quick(int v[],int esq,int dir){
         quick(v,pos+1,dir);
     }
 }
+int busca_b(int v[],int tam,int valor){
+    int ini=0,fim=tam-1,meio;
+    while(ini<=fim){
+        meio=(ini+fim)/2;
+        if(valor<v[meio]){
+            fim = meio-1;
+        }else if(valor>v[meio]){
+                ini=meio+1;
+                }else return meio;
+    }
+    return -1;
+}
 int main(void){
-    int v[cap] = {2,6,4,9,0};
+    setlocale(LC_ALL,"portuguese");
+    int vetor[cap] ={2,5,1,6,7};
     int i;
     printf("Vetor = [");
     for(i=0;i<cap;i++)
-        printf("%d ",v[i]);
+        printf("%d ",vetor[i]);
     printf("]");
-    quick(v,0,cap);
-    printf("\n\nVetor Ordenado= [");
+    quick(vetor,0,cap);
+    printf("Vetor Ordenado= [");
     for(i=0;i<cap;i++)
-        printf("%d ",v[i]);
+        printf("%d ",vetor[i]);
     printf("]");
+    int valor,b;
+    printf("\n\nQual número buscará? ");
+    scanf("%d",&valor);
+    b = busca_b(vetor,cap,valor);
+    if(b==-1)
+        printf("\nValor não encontrado!\n\n");
+    else
+        printf("\nSe encontra no índice %d°\n\n",b);
 }
